db5d6d31d9e2684d02c1d60fdcb15833b057edcc
ilantimerman <ilan.tmrmn@gmail.com>
2022-04-13 15:16:50 -0700

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 // set up express
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
